b7454c4ce77888ca1d0bab18e99e58d2328cc602
TakashiYano <0tp6324w3261z0v@gmail.com>
2021-06-22 07:51:34 +0000

    ✨ ヘッダーの追加

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,11 +1,13 @@
 import type { NextPage } from "next";
 import { Main } from "../components/Main";
 import { Footer } from "../components/shared/Footer";
+import { Header } from "../components/shared/Header";
 import styles from "../styles/Home.module.css";
 
 const About: NextPage = () => {
   return (
     <div className={styles.container}>
+      <Header />
       <Main page="about" />
       <Footer />
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import type { NextPage } from "next";
 import { Main } from "../components/Main";
 import { Footer } from "../components/shared/Footer";
+import { Header } from "../components/shared/Header";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
+      <Header />
       <Main page="index" />
       <Footer />
     </div>
