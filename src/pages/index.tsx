44f333fe00543ac6d6c93dcd20fb3f371912b14f
TakashiYano <0tp6324w3261z0v@gmail.com>
2021-06-23 07:04:01 +0000

    ✨ 簡単にuseStateで文字列と真偽値について実装

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,13 +7,26 @@ import styles from "src/styles/Home.module.css";
 
 const Home: NextPage = () => {
   const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
 
-  const handleClick = useCallback((e: React.MouseEvent<HTMLButtonElement>) => {
+  const handleClick = useCallback(() => {
     if (count < 10) {
       setCount(count => count + 1);
     }
-    e.preventDefault();
   }, [count]);
+
+  const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value.length > 5) {
+      alert("5文字以内にしてください");
+      return;
+    }
+    setText(e.target.value.trim());
+  }, []);
+
+  const handleDisplay = useCallback(() => {
+    setIsShow((isShow) => !isShow);
+  }, []);
 
   useEffect(() => {
     // mount
@@ -27,8 +40,10 @@ const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Header />
-      <h1>{count}</h1>
+      {isShow ? <h1>{count}</h1> : null}
       <button onClick={handleClick}>ボタン</button>
+      <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+      <input type="text" value={text} onChange={handleChange} />
       <Main page="index" />
       <Footer />
     </div>
