034fbd5360196634bf4938e2fa82afda99cb3bfa
TakashiYano <0tp6324w3261z0v@gmail.com>
2021-06-23 04:18:55 +0000

    ⚗️ 簡単なクリックイベントの実装

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,19 @@
 import type { NextPage } from "next";
+import { useCallback } from "react";
 import { Main } from "src/components/Main";
 import { Footer } from "src/components/shared/Footer";
 import { Header } from "src/components/shared/Header";
 import styles from "src/styles/Home.module.css";
 
 const Home: NextPage = () => {
+  const handleClick = useCallback((e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+  }, []);
+
   return (
     <div className={styles.container}>
       <Header />
+      <button onClick={handleClick}>ボタン</button>
       <Main page="index" />
       <Footer />
     </div>
