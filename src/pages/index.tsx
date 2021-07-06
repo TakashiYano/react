c16f9ff9d4bfeee540938750a92b2508ab2414ac
TakashiYano <0tp6324w3261z0v@gmail.com>
2021-07-06 13:37:39 +0000

    🚸 APIを叩く際のローディング状態・エラー状態を追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,13 @@
 import type { NextPage } from "next";
 import classes from "src/styles/Home.module.css";
 import { Header } from "src/components/shared/Header";
-import { useCallback, useEffect, useState } from "react";
+import { Posts } from "src/components/Posts";
 
 const Home: NextPage = () => {
-  const [posts, setPosts] = useState([]);
-
-  const getPosts = useCallback(async () => {
-    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-    const json = await res.json();
-    setPosts(json);
-  }, []);
-
-  useEffect(() => {
-    getPosts();
-  }, [getPosts]);
-
   return (
     <div className={classes.container}>
       <Header />
-      {posts.length > 0 ? (
-        <ol>
-          {posts.map(({ id, title }) => {
-            return <li key={id}>{title}</li>;
-          })}
-        </ol>
-      ) : null}
+      <Posts />
     </div>
   );
 };
