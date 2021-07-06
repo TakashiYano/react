f0994a63d73d31c547e84b39f38b1fe7e10dce99
TakashiYano <0tp6324w3261z0v@gmail.com>
2021-07-06 12:47:10 +0000

    ✨ 非同期処理 json-placeholderより取得

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,31 @@
 import type { NextPage } from "next";
 import classes from "src/styles/Home.module.css";
-import { Main } from "src/components/Main";
-import { Footer } from "src/components/shared/Footer";
 import { Header } from "src/components/shared/Header";
+import { useCallback, useEffect, useState } from "react";
 
-type Props = {
-  count: number;
-  isShow: boolean;
-  handleClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
-  handleDisplay: (event: React.MouseEvent<HTMLButtonElement>) => void;
-  text: string;
-  array: string[];
-  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  handleAdd: (event: React.MouseEvent<HTMLButtonElement>) => void;
-};
+const Home: NextPage = () => {
+  const [posts, setPosts] = useState([]);
 
-const Home: NextPage<Props> = (props) => {
+  const getPosts = useCallback(async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const json = await res.json();
+    setPosts(json);
+  }, []);
+
+  useEffect(() => {
+    getPosts();
+  }, [getPosts]);
+
   return (
     <div className={classes.container}>
       <Header />
-
-      {props.isShow ? <h1>{props.count}</h1> : null}
-      <button onClick={props.handleClick}>ボタン</button>
-      <button onClick={props.handleDisplay}>
-        {props.isShow ? "非表示" : "表示"}
-      </button>
-
-      <input type="text" value={props.text} onChange={props.handleChange} />
-      <button onClick={props.handleAdd}>追加</button>
-      <ul>
-        {props.array.map((item) => {
-          return <li key={item}>{item}</li>;
-        })}
-      </ul>
-
-      <Main page="index" />
-      <Footer />
+      {posts.length > 0 ? (
+        <ol>
+          {posts.map(({ id, title }) => {
+            return <li key={id}>{title}</li>;
+          })}
+        </ol>
+      ) : null}
     </div>
   );
 };
