26c575fa3f768b7bdc9b27654cad3a5a014671e5
TakashiYano <0tp6324w3261z0v@gmail.com>
2021-08-28 06:58:11 +0000

    🔥 Indexから不要なコードを削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,43 +1,15 @@
 import type { NextPage } from "next";
 import classes from "src/styles/Home.module.css";
-import { Main } from "src/components/Main";
-import { Footer } from "src/components/shared/Footer";
 import { Header } from "src/components/shared/Header";
 
-type Props = {
-  doubleCount: number;
-  isShow: boolean;
-  handleClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
-  handleDisplay: (event: React.MouseEvent<HTMLButtonElement>) => void;
-  text: string;
-  array: string[];
-  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  handleAdd: (event: React.MouseEvent<HTMLButtonElement>) => void;
-};
-
-const About: NextPage<Props> = (props) => {
+const Index: NextPage = () => {
   return (
     <div className={classes.container}>
       <Header />
-
-      {props.isShow ? <h1>{props.doubleCount}</h1> : null}
-      <button onClick={props.handleClick}>ボタン</button>
-      <button onClick={props.handleDisplay}>
-        {props.isShow ? "非表示" : "表示"}
-      </button>
-
-      <input type="text" value={props.text} onChange={props.handleChange} />
-      <button onClick={props.handleAdd}>追加</button>
-      <ul>
-        {props.array.map((item) => {
-          return <li key={item}>{item}</li>;
-        })}
-      </ul>
-
-      <Main page="about" />
-      <Footer />
+      <h1>title</h1>
+      <p>JSONPlaceholder</p>
     </div>
   );
 };
 
-export default About;
+export default Index;
