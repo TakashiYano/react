cf9e996af7b1d70bce7cba91e94ce611ed6696a9
TakashiYano <0tp6324w3261z0v@gmail.com>
2021-08-28 07:07:25 +0000

    🔥 不必要なコードやファイルを削除

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,11 @@
 import { AppProps } from "next/app";
 import toast, { Toaster, ToastBar } from "react-hot-toast";
 import "src/styles/globals.css";
-import { useBgColor } from "src/hooks/useBgColor";
-import { useCounter } from "src/hooks/useCounter";
-import { useInputArray } from "src/hooks/useInputArray";
-
 const App = (props: AppProps) => {
-  const counter = useCounter();
-  const inputArray = useInputArray();
-  useBgColor();
 
   return (
     <div>
-      <props.Component {...props.pageProps} {...counter} {...inputArray} />
+      <props.Component {...props.pageProps} />
       <Toaster>
         {(t) => (
           <ToastBar toast={t}>
