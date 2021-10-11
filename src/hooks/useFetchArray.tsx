8181f40b824b38a3ee102d56e193b0d74b59ddf8
TakashiYano <0tp6324w3261z0v@gmail.com>
2021-10-11 08:59:55 +0000

    ♻️ 配列のデータのAPIを叩く時の処理を共通化

---
+++ src/hooks/useFetchArray.tsx
@@ -0,0 +1,27 @@
+import { CommentsType } from "src/types/types";
+import useSWR, { Key } from "swr";
+
+export const useFetchArray = (url: Key) => {
+  const { data, error } = useSWR<CommentsType[], Error>(url);
+
+  return {
+    data,
+    error,
+    isLoading: !data && !error,
+    isEmpty: data && data.length === 0,
+  };
+};
+
+const API_URL = "https://jsonplaceholder.typicode.com";
+
+export const useComments = () => {
+  return useFetchArray(`${API_URL}/comments`);
+};
+
+export const usePosts = () => {
+  return useFetchArray(`${API_URL}/posts`);
+};
+
+export const useUsers = () => {
+  return useFetchArray(`${API_URL}/users`);
+};
