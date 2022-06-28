fe74bee4f0579306dfc93689b3a89afd8040f82b
yanot <0tp6324w3261z0v@gmail.com>
2022-06-28 15:18:22 +0900

    💄 Tailwind CSSを使って既存のスタイルの書き換え

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,6 +1,9 @@
-import classes from "src/components/Layout/Layout.module.css";
 import { LayoutType } from "src/types/types";
 
 export const Layout: React.FC<LayoutType> = (props) => {
-  return <div className={classes.container}>{props.children}</div>;
+  return (
+    <div className="flex flex-col items-center mx-auto px-2 min-h-screen max-w-2xl">
+      {props.children}
+    </div>
+  );
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import { AppProps } from "next/app";
 import { Layout } from "src/components/Layout";
 import "tailwindcss/tailwind.css";
-// import "src/styles/globals.css";
 const App = (props: AppProps) => {
   return (
     <>
