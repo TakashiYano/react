1324696d200d5f0c673236f32d08bf022708c933
yanot <0tp6324w3261z0v@gmail.com>
2022-06-28 14:20:26 +0900

    🔥 不要なコードを削除

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,30 +1,13 @@
 import { AppProps } from "next/app";
-import toast, { Toaster, ToastBar } from "react-hot-toast";
 import { Layout } from "src/components/Layout";
 import "src/styles/globals.css";
 const App = (props: AppProps) => {
   return (
-    <div>
+    <>
       <Layout>
         <props.Component {...props.pageProps} />
       </Layout>
-      <Toaster>
-        {(t) => (
-          <ToastBar toast={t}>
-            {({ icon, message }) => (
-              <>
-                {icon}
-                {message}
-                {t.type !== "loading" && (
-                  <button onClick={() => toast.dismiss(t.id)}>X</button>
-                )}
-              </>
-            )}
-          </ToastBar>
-        )}
-      </Toaster>
-      ;
-    </div>
+    </>
   );
 };
 
