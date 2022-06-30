20729f6d35c8c85b6d726dd8610d9b2b5b15d0b5
yanot <0tp6324w3261z0v@gmail.com>
2022-06-30 11:20:01 +0900

    ⚡️ コメントの個別ページをSG化

--- src/pages/comments/[id]/index.tsx
+++ src/pages/comments/[id]/index.tsx
@@ -1,12 +1,54 @@
-import type { NextPage } from "next";
+import type {
+  GetStaticPaths,
+  GetStaticPropsContext,
+  InferGetStaticPropsType,
+  NextPage,
+} from "next";
+import { ParsedUrlQuery } from "querystring";
 import { CommentComponent } from "src/components/Comment";
 import { Header } from "src/components/Header";
+import { CommentType } from "src/types/types";
+import { SWRConfig } from "swr";
 
-const CommentsId: NextPage = () => {
+interface Params extends ParsedUrlQuery {
+  id: string;
+}
+
+type Props = InferGetStaticPropsType<typeof getStaticProps>;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const comments = await fetch("https://jsonplaceholder.typicode.com/comments");
+  const commentsData: CommentType[] = await comments.json();
+  const paths = commentsData.map((comment) => ({
+    params: { id: comment.id.toString() },
+  }));
+
+  return { paths, fallback: false };
+};
+
+export const getStaticProps = async (ctx: GetStaticPropsContext) => {
+  const { id } = ctx.params as Params;
+  const COMMENT_API_URL = `https://jsonplaceholder.typicode.com/comments/${id}`;
+  const comment = await fetch(COMMENT_API_URL);
+  const commentData: CommentType = await comment.json();
+
+  return {
+    props: {
+      fallback: {
+        [COMMENT_API_URL]: commentData,
+      },
+    },
+  };
+};
+
+const CommentsId: NextPage<Props> = (props) => {
+  const { fallback } = props;
   return (
     <div>
       <Header />
-      <CommentComponent />
+      <SWRConfig value={{ fallback }}>
+        <CommentComponent />
+      </SWRConfig>
     </div>
   );
 };
